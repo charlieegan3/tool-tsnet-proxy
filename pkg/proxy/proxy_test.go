a91aa1154078c8c63b240f59f728609e211cf792
Charlie Egan <charlieegan3@users.noreply.github.com>
2024-06-14 09:54:14 +0100

    dry up test

--- pkg/proxy/proxy_test.go
+++ pkg/proxy/proxy_test.go
@@ -11,35 +11,21 @@ import (
 
 	"github.com/charlieegan3/tool-tsnet-proxy/pkg/httpclient"
 	dnstest "github.com/charlieegan3/tool-tsnet-proxy/pkg/test/dns"
+	"github.com/miekg/dns"
 )
 
 func TestSimple(t *testing.T) {
 	// example.com is the example upstream server host
 	const upstreamServerHost = "example.com"
 
-	const externaHost = "proxy.example.com"
-
-	// upstream server that is running behind the proxy
-	upstreamServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		for k, v := range r.Header {
-			fmt.Println(k, v)
-		}
-		_, err := w.Write([]byte("Hello, client"))
-		if err != nil {
-			t.Fatalf("Failed to write response: %s", err)
-		}
-	}))
-
-	upstreamServerURL, err := url.Parse(upstreamServer.URL)
-	if err != nil {
-		t.Fatalf("Failed to parse backend server URL: %s", err)
-	}
+	const proxyServerExternalHost = "proxy.example.com"
 
 	// this test dns server will map example.com to the loopback address
+	// where the test servers are running
 	dnsServer, err := dnstest.NewServer(dnstest.Options{
 		MappingsA: map[string]string{
-			upstreamServerHost + ".": "127.0.0.1",
-			externaHost + ".":        "127.0.0.1",
+			upstreamServerHost + ".":      "127.0.0.1",
+			proxyServerExternalHost + ".": "127.0.0.1",
 		},
 	})
 	if err != nil {
@@ -47,13 +33,19 @@ func TestSimple(t *testing.T) {
 	}
 	defer dnsServer.Shutdown()
 
-	// client that is configured to send all requests to the upstream server
-	upstreamServerClient := httpclient.NewUpsteamClient(httpclient.UpstreamClientOptions{
-		DNSNetwork: dnsServer.Net,
-		DNSAddr:    dnsServer.Addr,
-		Host:       upstreamServerHost,
-		Port:       upstreamServerURL.Port(),
+	// upstream server that is running behind the proxy
+	upstreamServerHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("Hello, client"))
+		if err != nil {
+			t.Fatalf("Failed to write response: %s", err)
+		}
 	})
+	upstreamServer, _, upstreamServerClient := newTestServerAndClient(
+		upstreamServerHost,
+		upstreamServerHandler,
+		dnsServer,
+	)
+	defer upstreamServer.Close()
 
 	// function that will match requests to the upstream servers
 	upstreamMatcher := func(req *http.Request) (*http.Client, bool) {
@@ -61,41 +53,33 @@ func TestSimple(t *testing.T) {
 			return nil, false
 		}
 
-		if !strings.HasPrefix(req.Host, externaHost) {
+		if !strings.HasPrefix(req.Host, proxyServerExternalHost) {
 			return nil, false
 		}
 
 		return upstreamServerClient, true
 	}
 
-	// create the proxy handler
+	// create the proxy handler and server
 	opts := &Options{
 		Matchers: []Matcher{upstreamMatcher},
 	}
 
-	h := NewHandler(opts)
-
-	// start the proxy server within an http test server
-	proxyServer := httptest.NewServer(h)
+	proxyServer, proxyServerURL, proxyServerClient := newTestServerAndClient(
+		proxyServerExternalHost,
+		NewHandler(opts),
+		dnsServer,
+	)
 	defer proxyServer.Close()
-
-	proxyServerURL, err := url.Parse(proxyServer.URL)
-	if err != nil {
-		t.Fatalf("Failed to parse proxy server URL: %s", err)
-	}
-
-	// client that is configured to send all requests to the proxy, regardless of the host
-	proxyServerClient := httpclient.NewUpsteamClient(httpclient.UpstreamClientOptions{
-		DNSNetwork: dnsServer.Net,
-		DNSAddr:    dnsServer.Addr,
-		Host:       externaHost,
-		Port:       proxyServerURL.Port(),
-	})
 
 	// make an example request to the proxy server
 	// the request will be matched based on the path. Host routing
 	// is not used as it didn't seem to work with httptest
-	req, err := http.NewRequest("GET", fmt.Sprintf("http://%s:%s/foobar", externaHost, proxyServerURL.Port()), nil)
+	req, err := http.NewRequest("GET", fmt.Sprintf(
+		"http://%s:%s/foobar",
+		proxyServerExternalHost,
+		proxyServerURL.Port(),
+	), nil)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -107,18 +91,42 @@ func TestSimple(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	assertStatusAndContent(t, resp, 200, "Hello, client")
+}
+
+func assertStatusAndContent(t *testing.T, resp *http.Response, status int, content string) {
 	bodyBs, err := io.ReadAll(resp.Body)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if resp.StatusCode != http.StatusOK {
+	if resp.StatusCode != status {
 		t.Log(string(bodyBs))
 
 		t.Fatalf("Expected status code 200, got %d", resp.StatusCode)
 	}
 
-	if string(bodyBs) != "Hello, client" {
+	if !strings.Contains(string(bodyBs), content) {
 		t.Fatalf("Expected 'Hello, client', got %s", string(bodyBs))
 	}
+}
+
+func newTestServerAndClient(
+	host string,
+	handler http.Handler,
+	dnsServer *dns.Server,
+) (*httptest.Server, *url.URL, *http.Client) {
+	s := httptest.NewServer(handler)
+
+	sURL, _ := url.Parse(s.URL)
+
+	// client that is configured to send all requests to the upstream server
+	c := httpclient.NewUpsteamClient(httpclient.UpstreamClientOptions{
+		DNSNetwork: dnsServer.Net,
+		DNSAddr:    dnsServer.Addr,
+		Host:       host,
+		Port:       sURL.Port(),
+	})
+
+	return s, sURL, c
 }
