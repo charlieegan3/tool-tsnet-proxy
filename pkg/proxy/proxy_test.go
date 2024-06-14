2c23df5c2f19eefa5803cb67d669d36386880409
Charlie Egan <charlieegan3@users.noreply.github.com>
2024-06-14 11:35:35 +0100

    Add another matcher in proxy

--- pkg/proxy/proxy_test.go
+++ pkg/proxy/proxy_test.go
@@ -14,18 +14,22 @@ import (
 	"github.com/miekg/dns"
 )
 
-func TestSimple(t *testing.T) {
+func TestProxyWithTwoMatchers(t *testing.T) {
 	// example.com is the example upstream server host
-	const upstreamServerHost = "example.com"
+	const upstreamServerHost1 = "1.example.com"
+	const upstreamServerHost2 = "2.example.com"
 
-	const proxyServerExternalHost = "proxy.example.com"
+	const externalHost1 = "ext1.example.com"
+	const externalHost2 = "ext2.example.com"
 
 	// this test dns server will map example.com to the loopback address
 	// where the test servers are running
 	dnsServer, err := dnstest.NewServer(dnstest.Options{
 		MappingsA: map[string]string{
-			upstreamServerHost + ".":      "127.0.0.1",
-			proxyServerExternalHost + ".": "127.0.0.1",
+			upstreamServerHost1 + ".": "127.0.0.1",
+			upstreamServerHost2 + ".": "127.0.0.1",
+			externalHost1 + ".":       "127.0.0.1",
+			externalHost2 + ".":       "127.0.0.1",
 		},
 	})
 	if err != nil {
@@ -33,60 +37,104 @@ func TestSimple(t *testing.T) {
 	}
 	defer dnsServer.Shutdown()
 
-	// upstream server that is running behind the proxy
-	upstreamServerHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// upstream servers that are running behind the proxy
+	upstreamServerHandler1 := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		_, err := w.Write([]byte("Hello, client"))
 		if err != nil {
 			t.Fatalf("Failed to write response: %s", err)
 		}
 	})
-	upstreamServer, _, upstreamServerClient := newTestServerAndClient(
-		upstreamServerHost,
-		upstreamServerHandler,
+	upstreamServer1, _, upstreamServerClient1 := newTestServerAndClient(
+		upstreamServerHost1,
+		upstreamServerHandler1,
 		dnsServer,
 	)
-	defer upstreamServer.Close()
+	defer upstreamServer1.Close()
 
 	// function that will match requests to the upstream servers
-	upstreamMatcher := func(req *http.Request) (*http.Client, bool) {
+	upstreamMatcher1 := func(req *http.Request) (*http.Client, bool) {
 		if req.URL.Path != "/foobar" {
 			return nil, false
 		}
 
-		if !strings.HasPrefix(req.Host, proxyServerExternalHost) {
+		if !strings.HasPrefix(req.Host, externalHost1) {
 			return nil, false
 		}
 
-		return upstreamServerClient, true
+		return upstreamServerClient1, true
+	}
+
+	// upstream servers that are running behind the proxy
+	upstreamServerHandler2 := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("Hello, client"))
+		if err != nil {
+			t.Fatalf("Failed to write response: %s", err)
+		}
+	})
+	upstreamServer2, _, upstreamServerClient2 := newTestServerAndClient(
+		upstreamServerHost2,
+		upstreamServerHandler2,
+		dnsServer,
+	)
+	defer upstreamServer2.Close()
+
+	// function that will match requests to the upstream servers
+	upstreamMatcher2 := func(req *http.Request) (*http.Client, bool) {
+		if req.URL.Path != "/foobar" {
+			return nil, false
+		}
+
+		if !strings.HasPrefix(req.Host, externalHost2) {
+			return nil, false
+		}
+
+		return upstreamServerClient2, true
 	}
 
 	// create the proxy handler and server
 	opts := &Options{
-		Matchers: []Matcher{upstreamMatcher},
+		Matchers: []Matcher{upstreamMatcher2, upstreamMatcher1},
 	}
 
 	proxyServer, proxyServerURL, proxyServerClient := newTestServerAndClient(
-		proxyServerExternalHost,
+		externalHost1,
 		NewHandler(opts),
 		dnsServer,
 	)
 	defer proxyServer.Close()
 
-	// make an example request to the proxy server
-	// the request will be matched based on the path. Host routing
-	// is not used as it didn't seem to work with httptest
+	// make an example request to the proxy server to upstreamServer1
 	req, err := http.NewRequest("GET", fmt.Sprintf(
 		"http://%s:%s/foobar",
-		proxyServerExternalHost,
+		externalHost1,
 		proxyServerURL.Port(),
 	), nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	req.Header.Set("Host", upstreamServerHost)
+	req.Header.Set("Host", upstreamServerHost1)
 
 	resp, err := proxyServerClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assertStatusAndContent(t, resp, 200, "Hello, client")
+
+	// make an example request to the proxy server to upstreamServer2
+	req, err = http.NewRequest("GET", fmt.Sprintf(
+		"http://%s:%s/foobar",
+		externalHost2,
+		proxyServerURL.Port(),
+	), nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req.Header.Set("Host", upstreamServerHost1)
+
+	resp, err = proxyServerClient.Do(req)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -101,13 +149,11 @@ func assertStatusAndContent(t *testing.T, resp *http.Response, status int, conte
 	}
 
 	if resp.StatusCode != status {
-		t.Log(string(bodyBs))
-
-		t.Fatalf("Expected status code 200, got %d", resp.StatusCode)
+		t.Errorf("Expected status code 200, got %d", resp.StatusCode)
 	}
 
 	if !strings.Contains(string(bodyBs), content) {
-		t.Fatalf("Expected 'Hello, client', got %s", string(bodyBs))
+		t.Errorf("Expected 'Hello, client', got %s", string(bodyBs))
 	}
 }
 
