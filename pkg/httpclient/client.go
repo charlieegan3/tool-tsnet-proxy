2d450383f97aefc8a0edd42215328191eba39b55
Charlie Egan <charlieegan3@users.noreply.github.com>
2024-06-24 10:15:24 +0100

    manually resolve dns

--- pkg/httpclient/client.go
+++ pkg/httpclient/client.go
@@ -31,50 +31,62 @@ func NewUpsteamClient(opts UpstreamClientOptions) *http.Client {
 		upstreamServerPort = "80"
 	}
 
-	upstreamServerAddrAndPort := fmt.Sprintf("%s:%s", upstreamServerHost, upstreamServerPort)
+	customResolver := &net.Resolver{
+		PreferGo: true,
+		Dial: func(_ context.Context, _, _ string) (net.Conn, error) {
+			var conn net.Conn
+			for _, dnsServer := range opts.DNSServers {
+				dnsNetwork := dnsServer.Network
+				if dnsNetwork == "" {
+					dnsNetwork = "tcp6"
+				}
 
-	customDialer := &net.Dialer{
-		Resolver: &net.Resolver{
-			PreferGo: true,
-			Dial: func(_ context.Context, _, _ string) (net.Conn, error) {
+				dnsAddr := dnsServer.Addr
+				if dnsAddr == "" {
+					dnsAddr = "[::1]:53"
+				}
+
+				var err error
+				conn, err = net.DialTimeout(dnsNetwork, dnsAddr, time.Second)
+				if err != nil {
+					return nil, fmt.Errorf("failed to dial dns server: %w", err)
+				}
+
+				if conn != nil {
+					break
+				}
+			}
+
+			if conn == nil {
+				return nil, errors.New("failed to dial all dns servers")
+			}
+
+			return conn, nil
+		},
+	}
+
+	dialer := net.Dialer{}
+
+	return &http.Client{
+		Transport: &http.Transport{
+			DialContext: func(ctx context.Context, network string, _ string) (net.Conn, error) {
+				ips, err := customResolver.LookupIP(ctx, "ip", upstreamServerHost)
+				if err != nil {
+					return nil, fmt.Errorf("failed to lookup IP: %w", err)
+				}
+
 				var conn net.Conn
-				for _, dnsServer := range opts.DNSServers {
-					dnsNetwork := dnsServer.Network
-					if dnsNetwork == "" {
-						dnsNetwork = "tcp6"
-					}
+				for _, ip := range ips {
+					upstreamServerAddrAndPort := net.JoinHostPort(ip.String(), upstreamServerPort)
 
-					dnsAddr := dnsServer.Addr
-					if dnsAddr == "" {
-						dnsAddr = "[::1]:53"
-					}
-
-					var err error
-					conn, err = net.DialTimeout(dnsNetwork, dnsAddr, time.Second)
+					conn, err = dialer.DialContext(ctx, network, upstreamServerAddrAndPort)
 					if err != nil {
-						return nil, fmt.Errorf("failed to dial dns server: %w", err)
+						return nil, fmt.Errorf("failed to dial upstream server: %w", err)
 					}
 
 					if conn != nil {
 						break
 					}
-				}
-
-				if conn == nil {
-					return nil, errors.New("failed to dial all dns servers")
-				}
-
-				return conn, nil
-			},
-		},
-	}
-
-	return &http.Client{
-		Transport: &http.Transport{
-			DialContext: func(ctx context.Context, network string, _ string) (net.Conn, error) {
-				conn, err := customDialer.DialContext(ctx, network, upstreamServerAddrAndPort)
-				if err != nil {
-					return nil, fmt.Errorf("failed to dial upstream server: %w", err)
 				}
 
 				return conn, nil
